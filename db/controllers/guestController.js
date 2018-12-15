00b55568740f100759c39d322372e6cb6e37d97f
Joscelyn James <josce.james7@hotmail.com>
2018-12-15 10:53:43 -0700

    added remove guest to controller

--- db/controllers/guestController.js
+++ db/controllers/guestController.js
@@ -1,79 +1,77 @@
-require('../models/guestModel');
-var mongoose = require('mongoose');
-var nodemailer = require('nodemailer');
-const fs = require('fs');
-var emailTemplate = require('../email/email');
+require("../models/guestModel");
+var mongoose = require("mongoose");
+var nodemailer = require("nodemailer");
+const fs = require("fs");
+var emailTemplate = require("../email/email");
 
-require('dotenv').config();
+require("dotenv").config();
 
 var transporter = nodemailer.createTransport({
-	service: 'gmail',
-	auth: {
-		user: 'jacqueandkemelwedding@gmail.com',
-		pass: process.env.PASSWORD
-	}
+  service: "gmail",
+  auth: {
+    user: "jacqueandkemelwedding@gmail.com",
+    pass: process.env.PASSWORD
+  }
 });
 
-var Guest = mongoose.model('Guests');
+var Guest = mongoose.model("Guests");
 
 exports.list_all_guests = (req, res) => {
+  Guest.find({}, (err, guest) => {
+    if (err) res.status(500).send(err);
 
-	Guest.find({}, (err, guest) => {
-
-		if (err) res.status(500).send(err);
-
-		res.status(200).json(guest);
-
-	});
-
+    res.status(200).json(guest);
+  });
 };
 
 exports.create_a_guest = (req, res) => {
+  if (!req.body.email || !req.body.guest1) {
+    res.status(500).send("Missing data");
+  } else {
+    var new_guest = new Guest(req.body);
+    console.log("before save");
+    new_guest.save((err, guest) => {
+      if (err) res.status(500).send(err);
 
-	if (!req.body.email || !req.body.guest1) {
+      var mailOptions;
 
-		res.status(500).send('Missing data');
+      if (guest.attending) {
+        console.log("in attending");
+        mailOptions = {
+          from: "jacqueandkemelwedding@gmail.com",
+          to: guest.email,
+          subject: "We have you in the books",
+          html: emailTemplate
+        };
+      } else {
+        mailOptions = {
+          from: "jacqueandkemelwedding@gmail.com",
+          to: guest.email,
+          subject: "We got your RSVP",
+          html: `<p>We are sorry to hear you cant make it!</p>`
+        };
+      }
 
-	} else {
+      console.log("before transporter");
 
-		var new_guest = new Guest(req.body);
-			console.log('before save')
-			new_guest.save((err, guest) => {
-		
-				if (err) res.status(500).send(err);
+      transporter.sendMail(mailOptions, function(err, info) {
+        if (err) {
+          console.log("Error in transporter: ", err);
+          res.status(500).send(err);
+        } else {
+          res.status(200).json(guest);
+        }
+      });
+    });
+  }
 
-				var mailOptions;
-
-				if (guest.attending) {
-					console.log('in attending')
-					mailOptions = {
-						from: 'jacqueandkemelwedding@gmail.com',
-						to: guest.email,
-						subject: 'We have you in the books',
-						html: emailTemplate
-					};
-				} else {
-					mailOptions = {
-						from: 'jacqueandkemelwedding@gmail.com',
-						to: guest.email,
-						subject: 'We got your RSVP',
-						html: `<p>We are sorry to hear you cant make it!</p>`
-					};
-				}
-
-				console.log('before transporter')
-
-				transporter.sendMail(mailOptions, function (err, info) {
-					console.log('in transporter')
-
-					if (err) res.status(500).send(err);
-		
-					res.status(200).json(guest);
-		
-				});
-		
-			});
-
-	}
-
+  remove_guest = id => {
+    mongoose.findOneAndRemove({ id: id }, (err, res) => {
+      if (err) {
+        res.status(550).send(err);
+      } else {
+        res.status(200).send("Guest Removed");
+      }
+    });
+  };
 };
